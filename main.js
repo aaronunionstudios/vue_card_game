febfbb946f6e04e05df09d00fe0f830b371eb42d
Aaron Wilson <aaron@unionstudios.com>
2021-12-30 11:07:51 -0500

    adding the data values for width ratio

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 new Vue ({
     name: 'game',
     el: '#app',
+
+    data: state,
+
+    template: `<div id="#app">
+    {{ worldRatio }}
+    </div>`,
+})
+window.addEventListener('resize', () => {
+    state.worldRatio = getWorldRatio ()
 })
